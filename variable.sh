a7e8c34a7d2ace49a13b8501fe89f2e5c7881890
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-20 14:13:40 +0530

    2

---
+++ variable.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# variable
+a=1
+# character
+b=xyz
+#bollean
+c=true
+#string
+d=abc123
+
+#use single and double quotes based on code
+
+x="2*3"
+
+echo a=$a
+echo b=${b}
+
+# date
+DATE=$(date)
+echo welcome,todaydate is $DATE
+
+ADD=$((2+3))
+echo ADD=$ADD
