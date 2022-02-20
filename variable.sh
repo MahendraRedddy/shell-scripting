ced6caf93bed0c51be614901963d495244cc6ee8
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-20 14:15:02 +0530

    2

--- variable.sh
+++ variable.sh
@@ -11,14 +11,4 @@ d=abc123
 
 #use single and double quotes based on code
 
-x="2*3"
-
-echo a=$a
-echo b=${b}
-
-# date
-DATE=$(date)
-echo welcome,todaydate is $DATE
-
-ADD=$(2+3)
-echo ADD =$ADD
+#echo ADD =$ADD
