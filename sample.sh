97bd77ed4f18a11acfa17c5663e6d7a8fdbc0952
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-20 12:18:40 +0530

    2

--- sample.sh
+++ sample.sh
@@ -8,6 +8,6 @@ netstat -lntp
 echo -e "mahe\nmahe2"
 echo -e "welcome\twelcome2"
 echo -e "\e[31mmahendra in redcolor"
-echo -e "\e[33mmahesh in bluecolor"
+echo -e "\e[34mmahesh in bluecolor"
 
-echo -e "\e[31mhello\n32mworld\tBye"
+echo -e "\e[32mhello\nworld\tBye"
