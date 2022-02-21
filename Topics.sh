80b0ff4fdcf8d26de11f152a6585a7e800b45f76
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-21 20:03:22 +0530

    2

---
+++ Topics.sh
@@ -0,0 +1,36 @@
+ In a shell we have multiple shells,out of that we are going to use "BASH"
+ BASH=SH+KSH+CSH one server side BASH is default
+ ZSH=To much modern,but most on desktop environment,Not on server
+
+a.BASH is default in Redhat family most
+b.BASH features of SH,KSH,CSH
+c.In redhat family os SH and BASH are same
+d.Shell scripting file ends with .sh or .bash extension
+     .sh
+     .bash
+e.Extensions are only for our understanding,usually we make .sh as extension for shell scripting files.
+f.In generally start with location /bin/bash
+
+ Topics are going to discuss
+ 1.SHE-BANG and Comments
+ 2.print Message on screen
+ 3.Variables
+ 4.Read the input users
+ 5.Functions
+ 6.Redirectors,quotes,exit states
+ 7.Conditions
+ 8.Loops
+ 9.SED Editor
+
+
+   is called #! as SHE-BANG,it denotes path of interpreter and also ensure script excutes with that interpreter.if
+   you script with ./script.sh instead of bash script.sh
+
+   Any line starts with # is a comment and lines will be ignored during execution
+
+   in putty terminal using
+   Git clone ssh url=first attempt
+   Git push ssh url=Changes in code
+
+   in putty to excute below command to use
+   bash filename.sh
