9ad1fca574ad90ea3e381f5958cb2a0158e56801
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-21 15:53:08 +0530

    1

--- variable.sh
+++ variable.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # variable
-a=1
+ a=1
 # character
-b=xyz
+ b=xyz
 #bollean
 c=true
 #string
 d=abc123
 
 #use single and double quotes based on code
+echo a=$a
+echo b=$("b")
 
+echo c=$("c")
+
+echo d=$("d")
 #echo ADD =$ADD
