df409f06ceb68cfd47b861b03f98a977ccd7dc78
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-21 16:50:06 +0530

    2

--- variable.sh
+++ variable.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-
-# variable
- a=1
-# character
- b=xyz
-#bollean
-c=true
-#string
-d=abc123
-
-#use single and double quotes based on code
-
-echo value of a= $a
-
-echo value of b= $(("b"))
-
-
-
-#echo ADD =$ADD
+  #varibale
+  a=10
+  echo value of a=$a
