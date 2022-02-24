b99353f3a3d793109ee8bea59fd5b9523e825677
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-24 13:03:23 +0530

    1

--- script.sh
+++ script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# 0 is script name
 echo 0=$0
+# 1 is first argument to script
 echo 1=$1
+# 2 is second argument to script
 echo 2=$2
+
+# @ and * is giving all aruguments
 echo "@=$@"
 echo "*=$*"
+#  # is No of values which are parsed
 echo "#=$#"
 
+echo -e "Your Name=$1\nYour Age=$2"
