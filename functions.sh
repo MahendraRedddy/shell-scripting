8948eb6f83976b4d642dbba1e0a9c4fa2a0a7159
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-24 18:00:59 +0530

    2

---
+++ functions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Functions should be always declared using it same like variable
+# so that is reason,function we always find in starting of scripts
+
+functions abc() {
+  echo iam student abc
+}
