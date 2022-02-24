ed76319a8d7e11a6a82c512194db2be69e932b52
Mahendra Reddy <eedalamahendra@gmail.com>
2022-02-24 18:01:52 +0530

    1

--- functions.sh
+++ functions.sh
@@ -2,6 +2,6 @@
 #Functions should be always declared using it same like variable
 # so that is reason,function we always find in starting of scripts
 
-functions abc() {
+function abc() {
   echo iam student abc
 }
